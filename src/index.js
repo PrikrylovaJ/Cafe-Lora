b70158cb44450a90cc018713a007164ab7f71137
PrikrylovaJ <jprikrylova@email.cz>
2021-05-01 11:37:23 +0200

    Objednávání nápoje-----------

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 
 console.log('funguju!');
 
+// -------------------Navigace----------------------
 
 const btnElm = document.querySelector('#nav-btn');
 const navElm = document.querySelector('nav');
@@ -21,3 +22,25 @@ for (let i = 0; i < aElements.length; i++) {
   const aElm = aElements[i];
   aElm.addEventListener('click', navigace);
 }
+
+// ---------------------Objednávání  nápoje-----------------------------
+
+const objElm = document.querySelector('.order-btn');
+const drinkElm = document.querySelector('.drink__cup');
+let ordered = false;
+
+const order = () => {
+    if (ordered === false) {
+    console.log('objednat');
+    drinkElm.classList.toggle('drink__cup--selected');
+    objElm.textContent = "Zrušit";
+    ordered = true;
+  } else {
+    console.log('zrusit');
+    drinkElm.classList.toggle('drink__cup--selected');
+    objElm.textContent = "Objednat";
+    ordered = false;
+  }
+}
+
+objElm.addEventListener('click', order);
