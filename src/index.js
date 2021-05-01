a6a7811be40ab67b34545552367cde53dcbe3d49
PrikrylovaJ <jprikrylova@email.cz>
2021-05-01 11:11:19 +0200

    Zprovoznění navigace-odkazů

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './style.css';
 
 console.log('funguju!');
+
+
+const btnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+
+btnElm.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+})
+
+const aElements = document.querySelectorAll('a');
+
+const navigace = () => {
+  const navElm = document.querySelector('nav');
+  navElm.classList.toggle('nav-closed');
+}
+
+for (let i = 0; i < aElements.length; i++) {
+  const aElm = aElements[i];
+  aElm.addEventListener('click', navigace);
+}
